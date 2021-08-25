b9d78b9ea582d2bf145e6e1303232ce5932118b3
Pankaj <pankajsurti@users.noreply.github.com>
2021-08-25 11:35:20 -0500

    The first draft

---
+++ src/extensions/sp2010AlertBannerAppExtention/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ISp2010AlertBannerAppExtentionApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'Sp2010AlertBannerAppExtentionApplicationCustomizerStrings' {
+  const strings: ISp2010AlertBannerAppExtentionApplicationCustomizerStrings;
+  export = strings;
+}
